9ad3a50eeb3913baac4de426b17f3669e56623ca
Cyrus Kiprop <cyruskiprop254@gmail.com>
2021-01-26 16:29:23 +0300

    add handler function to the dropdown and line elements of the svg

--- src/controllers/impact_graphs_controller.js
+++ src/controllers/impact_graphs_controller.js
@@ -8,4 +8,21 @@ export default class ImpactGraphs extends Controller {
   };
 
   connect() {}
+
+  // handler functions  && eventListeners
+  handleBarMouseover(event) {
+    const target = event.target;
+    const index = Number(target.getAttribute('id'));
+    this.tooltipTargets[index].style.display = 'block';
+  }
+
+  handleBarMouseleave(event) {
+    const target = event.target;
+    const index = Number(target.getAttribute('id'));
+    this.tooltipTargets[index].style.display = 'none';
+  }
+
+  handleFilter(event) {
+    this.updateUserInterface(event.target.value);
+  }
 }
