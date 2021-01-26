546c410ec09486aab718e18e6707787ec1b930ef
Cyrus Kiprop <cyruskiprop254@gmail.com>
2021-01-26 15:16:38 +0300

    install and configure eslint packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    bundle: "./src/index.js",
+    bundle: './src/index.js',
   },
 
   output: {
-    filename: "[name].js",
-    path: path.resolve(__dirname, "public"),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public'),
   },
 
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
 
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: [/node_modules/],
-        use: [{ loader: "babel-loader" }],
+        use: [{ loader: 'babel-loader' }],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
     ],
   },
