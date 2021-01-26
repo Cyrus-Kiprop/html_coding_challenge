cae3e9176abcec01042cd4ff845e41ad279d197a
Cyrus Kiprop <cyruskiprop254@gmail.com>
2021-01-26 14:41:32 +0300

    configure webpack and babel for transpiling purpose

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: "./src/index.js",
+  },
+
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "public"),
+  },
+
+  mode: "production",
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{ loader: "babel-loader" }],
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+    ],
+  },
+};
