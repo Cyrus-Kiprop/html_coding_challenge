af2e8aa4168f2ec90d2964de84e0dcad87ea5435
Cyrus Kiprop <cyruskiprop254@gmail.com>
2021-01-26 18:54:22 +0300

    update mock_api test

--- test/javascript/api/mock_api_request_test.js
+++ test/javascript/api/mock_api_request_test.js
@@ -1,10 +1,7 @@
 import mockApiRequest from '../../../src/api/mockApiRequest';
 import jsonData from '../../../src/db/data.json';
 
-test('the api request should return a promise object that resolves to a json data', () => mockApiRequest().then((data) => {
-  data.json().then((result) => {
-    expect(result)
-      .toBe(jsonData)
-      .catch((e) => expect(e).toMatch('error'));
-  });
-}));
+test('the api request should return a promise object that resolves to a json data', () =>
+  mockApiRequest().then((result) => {
+    expect(result).toBe(jsonData);
+  }));
