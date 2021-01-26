ef0d13fd65e16566b78b1760ec4742a0dfbecfb5
Cyrus Kiprop <cyruskiprop254@gmail.com>
2021-01-26 16:00:29 +0300

    add Test suite for mock_api_request

---
+++ test/javascript/api/mock_api_request_test.js
@@ -0,0 +1,12 @@
+import mockApiRequest from '../../../src/api/mockApiRequest';
+import jsonData from '../../../src/db/data.json';
+
+test('the api request should return a promise object that resolves to a json data', () => {
+  return mockApiRequest().then((data) => {
+    data.json().then((result) => {
+      expect(result)
+        .toBe(jsonData)
+        .catch((e) => expect(e).toMatch('error'));
+    });
+  });
+});
